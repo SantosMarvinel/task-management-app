53816ba96e5c2faac9f29f68100707faf950287e
Marvinel Santos <santos.marvinel@gmail.com>
2025-03-31 15:15:54 +0800

    persist data to localstorage

--- store/use-task-store.ts
+++ store/use-task-store.ts
@@ -2,45 +2,50 @@ import { nanoid } from "nanoid";
 import { TaskData } from "@/db/dummy-data";
 import { TaskStore } from "@/types/type";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
+export const useTaskStore = create<TaskStore>()(
+  persist(
+    (set) => ({
+      task: TaskData,
+      addTask: (title, description) =>
+        set((state) => ({
+          task: [
+            ...state.task,
+            {
+              id: nanoid(),
+              title,
+              description,
+              status: "Pending",
+            },
+          ],
+        })),
 
-export const useTaskStore = create<TaskStore>()((set) => ({
-  task: TaskData,
-  addTask: (title, description) =>
-    set((state) => ({
-      task: [
-        ...state.task,
-        {
-          id: nanoid(),
-          title,
-          description,
-          status: "Pending",
-        },
-      ],
-    })),
+      isCompleted: (id) =>
+        set((state) => ({
+          task: state.task.map((data) =>
+            data.id === id
+              ? {
+                  ...data,
+                  status: data.status === "Pending" ? "Completed" : "Pending",
+                }
+              : data
+          ),
+        })),
 
-  isCompleted: (id) =>
-    set((state) => ({
-      task: state.task.map((data) =>
-        data.id === id
-          ? {
-              ...data,
-              status: data.status === "Pending" ? "Completed" : "Pending",
-            }
-          : data
-      ),
-    })),
+      deleteTask: (id) =>
+        set((state) => ({
+          task: state.task.filter((data) => data.id !== id),
+        })),
 
-  deleteTask: (id) =>
-    set((state) => ({
-      task: state.task.filter((data) => data.id !== id),
-    })),
-
-  updateTask: (id, title, description) =>
-    set((state) => ({
-      task: state.task.map((data) =>
-        data.id === id
-          ? { ...data, title: title, description: description }
-          : data
-      ),
-    })),
-}));
+      updateTask: (id, title, description) =>
+        set((state) => ({
+          task: state.task.map((data) =>
+            data.id === id
+              ? { ...data, title: title, description: description }
+              : data
+          ),
+        })),
+    }),
+    { name: "task-data" }
+  )
+);
